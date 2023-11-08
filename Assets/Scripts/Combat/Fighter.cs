689b1698bf422f4c420566d4e996399e0acdf92d
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-11-08 20:54:03 +0700

    Enemy AI, Base Player

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour, IAction
+{
+    [SerializeField] float weaponRange = 2f;
+    [SerializeField] float timeBetweenAttack = 1f;
+    [SerializeField] float weaponDamage = 5f;
+
+    float timeSinceLastAttack = Mathf.Infinity;
+    
+    Health target;
+    Mover mover;
+
+    void Start()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    void Update()
+    {
+        timeSinceLastAttack += Time.deltaTime;
+
+        if (target == null) return;
+
+        if (target.IsDead()) return;
+
+        if (!GetIsInRange())
+        {
+            mover.MoveTo(target.transform.position, 1f);
+        }
+
+        else
+        {
+            mover.Cancel();
+            AttackBehaviour();
+        }
+    }
+
+    private void AttackBehaviour()
+    {
+        // transform.LookAt(target.transform);
+
+        if (timeSinceLastAttack > timeBetweenAttack)
+        {
+            // // This will trigger Hit() event.
+            // TriggerAttack();
+            target.TakeDamage(weaponDamage);
+            timeSinceLastAttack = 0;
+        }
+    }
+
+    // private void TriggerAttack()
+    // {
+    //     GetComponent<Animator>().ResetTrigger("stopAttack");
+    //     GetComponent<Animator>().SetTrigger("attack");
+    // }
+
+    // // Animation Event
+    // void Hit()
+    // {
+    //     if (target == null) return;
+    //     target.TakeDamage(weaponDamage);
+    // }
+
+    private bool GetIsInRange()
+    {
+        return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+    }
+
+    public float GetWeaponRange()
+    {
+        return weaponRange;
+    }
+
+    public bool CanAttack(GameObject combatTarget)
+    {
+        if (combatTarget == null) return false;
+        Health targetToTest = combatTarget.GetComponent<Health>();
+        return targetToTest != null && !targetToTest.IsDead();
+    }
+
+    public void Attack(GameObject combatTarget)
+    {
+        GetComponent<ActionScheduler>().StartAction(this);
+        target = combatTarget.GetComponent<Health>();
+    }
+
+    public void Cancel()
+    {
+        // TriggerStopAttack();
+        target = null;
+        GetComponent<Mover>().Cancel();
+    }
+
+    // private void TriggerStopAttack()
+    // {
+    //     GetComponent<Animator>().ResetTrigger("attack");
+    //     GetComponent<Animator>().SetTrigger("stopAttack");
+    // }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, weaponRange);
+        
+    }
+
+}
---
+++ Assets/Scripts/Core/Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float healthPoints = 100f;
+    bool isDead;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthPoints = Mathf.Max(healthPoints - damage, 0);
+        print(healthPoints);
+        if (healthPoints == 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        // GetComponent<Animator>().SetTrigger("die");
+        // GetComponent<ActionScheduler>().CancelCurrentAction();
+    }
+}
