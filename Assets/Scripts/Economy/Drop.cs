99fdda254e9ea3ca87b31676e69c932b6fbedecf
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-11-12 21:53:49 +0700

    Setting Drop

--- Assets/Scripts/Economy/Drop.cs
+++ Assets/Scripts/Economy/Drop.cs
@@ -2,56 +2,33 @@ using UnityEngine;
 
 public class Drop : MonoBehaviour {
 
-    [Header("Read-Only")]
-    [SerializeField] private float crystalChance;
-    [SerializeField] private float soulChance;
-    [SerializeField] private float noneChance;
+    [Header("Customize Drop")]
+    [Range(0, 1)][SerializeField] private float crystalChance = 1f;
+    [SerializeField] private int crystalDropAmount = 1;
+
+    [SerializeField] private int soulDropAmount = 1;
 
     GameObject soulPrefab;
     GameObject crystalPrefab;
 
     int sumWeigth;
 
-    [Header("Customize Drop")]
-    [Range(0, 100)]
-    [SerializeField] private int crystalWeigth;
-    [Range(0, 100)]
-    [SerializeField] private int soulWeigth;
-    [Range(0, 100)]
-    [SerializeField] private int noneWeigth;
-    [SerializeField] private int dropAmount;
-
-    private void GetSumWeigth()
-    {
-        sumWeigth = crystalWeigth + soulWeigth + noneWeigth;
-    }
-
     public void DropResource()
     {
-        GetSumWeigth();
-        int randomNum = Random.Range(0, sumWeigth);
+        TextPopup.CreateSoul(transform.position, soulDropAmount);
+        SoulManager.Instance.IncreaseSoul(soulDropAmount);
 
-        if (randomNum <= crystalWeigth)
-        {
-            TextPopup.CreateSoul(transform.position, dropAmount);
-            CrystalManager.Instance.IncreaseCrystal(dropAmount);
-        }
+        float randomChance = GetRandomChance();
 
-        else if (randomNum > crystalWeigth && randomNum <= soulWeigth + crystalWeigth)
+        if (randomChance < crystalChance)
         {
-            TextPopup.CreateCrystal(transform.position, dropAmount);
-            SoulManager.Instance.IncreaseSoul(dropAmount);
+            TextPopup.CreateCrystal(transform.position, crystalDropAmount);
+            CrystalManager.Instance.IncreaseCrystal(crystalDropAmount);
         }
     }
 
-    void OnValidate() {
-        GetSumWeigth();
-        crystalChance = (float)crystalWeigth / (float)sumWeigth;
-        soulChance = (float)soulWeigth / (float)sumWeigth;
-        noneChance = (float)noneWeigth / (float)sumWeigth;
+    private float GetRandomChance()
+    {
+        return Random.Range(0f, 1f);
     }
-
-    
-
-
 }
